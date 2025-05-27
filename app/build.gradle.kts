5ec46c48d91c41d5234423c8e7adaf510647a6ec
Davidpham310 <truongphamdz2le3@gmail.com>
2025-05-28 03:57:21 +0700

    Secon commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+//    id("org.jetbrains.kotlin.kapt")
 }
 
 android {
@@ -27,21 +28,24 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
+//    implementation(libs.dagger.hilt.android)
+//    kapt(libs.dagger.hilt.compiler)
+    
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/kidlearn/MainActivity.kt
+++ app/src/main/java/com/example/kidlearn/MainActivity.kt
@@ -3,18 +3,24 @@ package com.example.kidlearn
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+//import dagger.hilt.android.AndroidEntryPoint
+//import javax.inject.Inject
 
+//@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+//    @Inject
+//    lateinit var greeting: String
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        enableEdgeToEdge()
+        
+//        // Safe access to greeting
+//        if (::greeting.isInitialized) {
+//            println("Greeting: $greeting")
+//        } else {
+//            println("Greeting is not initialized")
+//        }
     }
 }
